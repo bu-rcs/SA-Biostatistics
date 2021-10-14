cda5492c2b79a174c6e2b8002a04b9ce7296d775
Hanfei Xu <hfxu@scc4.bu.edu>
2021-10-14 16:08:19 -0400

    Rmarkdown sample code

--- examples/R/README.md
+++ examples/R/README.md
@@ -1,1 +1,16 @@
 # SCC Examples using the R language
+
+This folder currently contains following code:
+
+1) An example of runnig regular R batch job
+- Rcode_regular.R
+
+- qsub_regular.sh
+
+2) An example of running array job using R
+- Rcode_arrayjob.R
+
+- qsub_arrayjob.sh
+
+
+Please check the wiki page (Section 2.3 Running Batch Jobs) to see more description.
---
+++ examples/Rmarkdown/qsub_rmd.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -l
+#
+# Run this file using 'qsub qsub_rmd.sh'
+#
+# All lines starting with "#$" are SGE qsub commands
+#
+# If you want to omit some of the qsub commands, just remove "$" so that this line becomes a comment
+
+
+# Specify which project
+#$ -P projectname
+
+# To use buy-in nodes of the project
+#$ -l buyin
+
+# multiple slots
+#$ -pe omp 4
+
+# Give this job a name
+#$ -N jobname
+
+# Join standard output and error to a single file
+#$ -j y
+
+# Send an email when job ends running or has problem
+#$ -m eas
+
+# Whom to send the email to, by default the email will be sent to person who submitted this job, specifing email below will overwrite the default
+#$ -M aaa@bu.edu
+
+#Request 24 hours for the job, by default this will be 12 hours. Request more hours only if you need more hours, because the longer time you specify, the less nodes will be available for you.
+#$ -l h_rt=24:00:00
+
+# Now let's keep track of some information just in case anything goes wrong
+
+echo "=========================================================="
+echo "Starting on       : $(date)"
+echo "Running on node   : $(hostname)"
+echo "Current directory : $(pwd)"
+echo "Current job ID    : $JOB_ID"
+echo "Current job name  : $JOB_NAME"
+echo "Task index number : $TASK_ID"
+echo "=========================================================="
+
+# We recommend specify R version here, since the default R version will change.
+module load R/4.0.5
+
+# use R knitr package to knit sample.rmd
+R -e "knitr::stitch_rmd('sample.Rmd')"
+
+echo "=========================================================="
+echo "Finished on       : $(date)"
+echo "=========================================================="
